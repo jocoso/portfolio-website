228a4e850db56f8daaf050e5aaac639a948e5688
jocoso <joshua.collado022@gmail.com>
2024-08-23 12:54:34 -0400

    Created and implemented a header, a navigation bar and a footer

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import "./App.css";
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { Header, Footer } from './components';
 import { Homepage } from './pages';
 
 
 function App() {
     return(
       <Router>
+        <Header /> {/* HEADER */}
         <Routes>
           <Route exact path="/" Component={Homepage} />
         </Routes>
+        <Footer />
       </Router>
     )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <section id="footer-section">
+            All rights reserved
+        </section>
+    );
+}
+
+export default Footer;
