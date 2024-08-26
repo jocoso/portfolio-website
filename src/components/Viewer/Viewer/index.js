745bf869a5792cdd14d378f3b3a3358d958af629
jocoso <joshua.collado022@gmail.com>
2024-08-25 23:54:51 -0400

    Website displays slides dynamically.

--- src/components/Viewer/Viewer/index.js
+++ src/components/Viewer/Viewer/index.js
@@ -6,7 +6,6 @@ class Reel {
         this.slides = slides; // Array of objects {element, path}
         this.path = window.location.pathname; // String
         this.search(this.path); 
-        
     }
 
     // private:
@@ -15,24 +14,19 @@ class Reel {
     }
 
     search(path) {
+
         const naked_path = path.replace(/\//g, ''); // Removes forward slashes
+
+        // - Find the first slide in slides array that matches the same path as 
+        // the one given as a parameter.
+        const currentSlide = this.slides.filter((slide) => 
+            slide.path.replace(/\//g, '') === naked_path)[0];
         
-        // Search in an array if two items are the same
-        
-        // Go through the array
-        // If item1 === item2 then return the item
+        this.element = currentSlide.element; // Component
 
-
-        const currentSlide = this.slides.map(slide => {
-            if(slide.path.replace(/\//g, '') === naked_path) {
-                return slide;
-            }
-        });
-
-        this.setState("element", currentSlide.element); // Component
     }
-
-    // public:
+    
+    //! Event Listener for Insert
     pull_lever() {
         if(this.has_path_changed()) {
             try {
@@ -45,27 +39,28 @@ class Reel {
         }
     }
 
+    // public:
+    insert() {
+        window.addEventListener("popstate", (event) => {
+            this.pull_lever();
+        })
+        
+    }
+    
+    
     show() {
-        return this.element;
+        return this.element || <div>Failed</div>;
     }
 }
 
 // Create consistency in the page.
 // it will take a reel component as child
 // TODO: Change this function to take a reel instead of the slides.
-export default function Viewer({ slides=[] }) {
-    
-    // Scenes
-    const links = [
-        {name:"About Me",   path: "/path"      , element: }, 
-        {name: "Projects",  path: "/projects"  }, 
-        {name: "Art",       path: "/art"       }, 
-        {name: "Resume",    path: "/resume"    }, 
-        {name: "Say Hello", path: "/contact-me"},
-    ];
+export default function Viewer({ slides=[{}] }) {
 
-
-    const reel = Reel()
+    // TODO: For security reasons do this in App.js
+    const reel = new Reel(slides);
+    reel.insert();
 
     return(
         
@@ -74,16 +69,16 @@ export default function Viewer({ slides=[] }) {
             <div id="navigation-bar" className="min-w-full w-full min-h-20 sm:h-19 md:h-19 lg:h-20 flex rounded-t-lg bg-gray-300 shadow-lg">
                 <ul className="w-full flex flex-col sm:flex-row items-center text-center justify-center sm:space-x-7 lg:space-x-10 list-none">
                     {
-                        links.map((link) => {
-                            return(<li key={link.route} className="sm:w-30 w-full h-full text-center flex justify-center items-center relative group">
-                                <a href={link.route} className="p-3 text-base md:text-lg lg:text-xl font-semibold relative z-10 transition-all duration-300 group-hover:text-shadow-xl group-hover:text-highlightTwo">{link.name}</a>
+                        slides.map((slide) => {
+                            return(<li key={slide.route} className="sm:w-30 w-full h-full text-center flex justify-center items-center relative group">
+                                <a href={slide.route} className="p-3 text-base md:text-lg lg:text-xl font-semibold relative z-10 transition-all duration-300 group-hover:text-shadow-xl group-hover:text-highlightTwo">{slide.name}</a>
                                 <span className="w-0 h-full group-hover:w-full absolute inset-0 transition-all duration-300 group-hover:shadow-lg shadow-highlightOne bg-highlightOne"></span>
                             </li>)
                         })
                     }
                 </ul>
             </div>
-            
+            {reel.show()}
         </div>
     
     );
