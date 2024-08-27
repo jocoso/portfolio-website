b858ef7cbaa26c3437194b7adbde92e40f664e9a
jocoso <joshua.collado022@gmail.com>
2024-08-27 19:57:42 -0400

    Fixed #28 and other tailwindcss related issues

--- src/components/Viewer/Viewer/index.js
+++ src/components/Viewer/Viewer/index.js
@@ -30,7 +30,7 @@ class Reel {
         // - Find the first slide in slides array that matches 
         // the parameter.
         const currentSlide = this.slides.filter((slide) => 
-            slide.path.replace(/\//g, '') === naked_path)[0] || <div></div>;
+            slide.path.replace(/\//g, '') === naked_path)[0] || this.error;
         
         // A pseudo return
         this.element = currentSlide.element; // Component
@@ -81,28 +81,50 @@ class Reel {
 // it will take a reel component as child
 export default function Viewer({ slides=[{}] }) {
     const myInputRef = useRef(null);
+    const footerRef = useRef(null);
 
     useEffect(() => {
         if(myInputRef.current) {
             myInputRef.current.focus();
         }
-    })
+    });
+
+    // Ensures the footer is always at the bottom of the page
+    useEffect(() => {
+        if(footerRef.current) {
+            
+            const updateFooterPosition = () => {
+                footerRef.current.style.top = `${window.innerHeight - footerRef.current.offsetHeight}px`
+            };
+            updateFooterPosition();
+            window.addEventListener('resize', updateFooterPosition);
+
+            return () => {
+                window.removeEventListener('resize', updateFooterPosition);
+            };
+        }
+    }, [footerRef]);
+
+    
 
     // Put the slides in the Reel and then view
     const reel = new Reel(slides);
     reel.insert();
 
     return(
-        
-        <div id="viewer-container" className="w-11/12 min-w-8/12 lg:w-8/12 rounded-t-2xl flex flex-col flex-grow bg-white my-5 rounded-xl">
+        <div id="viewer-container" className="flex flex-col min-h-20 bg-white rounded-xl shadow-md">
             {/* NAVBAR */}
-            <div id="navigation-bar" className="min-w-full w-full min-h-20 sm:h-19 md:h-19 lg:h-20 flex rounded-t-lg bg-gray-300 shadow-lg">
-                <ul className="w-full flex flex-col sm:flex-row items-center text-center justify-center sm:space-x-7 lg:space-x-10 list-none">
+            <div id="navigation-bar" className=" bg-gray-300 shadow-lg rounded-t-lg">
+                <ul className="flex flex-row flex-wrap justify-center">
                     {
                         slides.map((slide) => {
-                            return(<li key={slide.path} className="sm:w-30 w-full h-full text-center flex justify-center items-center relative group">
-                                <a href={slide.path}  className="p-3 text-base md:text-lg lg:text-xl font-semibold relative z-10 transition-all duration-300 group-hover:text-shadow-xl group-hover:text-highlightTwo" >{slide.name}</a>
-                                <span className="w-0 h-full group-hover:w-full absolute inset transition-all duration-300 group-hover:shadow-lg shadow-highlightOne bg-highlightOne"></span>
+                            return(<li key={slide.path} className="group relative h-full min-w-10 p-5">
+                                
+                                <span className="absolute inset-y-0 bottom-0 left-1/2 w-0 h-full bg-highlightOne transition-all duration-300 ease-out transform -translate-x-1/2 group-hover:w-full"></span>    
+                                <a href={slide.path} className="relative text-2xl font-semibold group-hover:text-highlightTwo" >
+                                    {slide.name}    
+                                </a>
+                                
                             </li>)
                         })
                     }
@@ -111,17 +133,20 @@ export default function Viewer({ slides=[{}] }) {
             {/* == */}
 
             {reel.view()} {/* The Reel component manages this one */}
-
+            <div className="h-96">
+                    a
+            </div>
             {/* FOOTER */}
-            <d id="footer" className="w-full h-20 sm:h-19 md:h-19 lg:h-20 rounded-b-lg bg-gray-300 shadow-lg flex justify-center items-center relative bottom-0">
-                    <a href="https://www.linkedin.com/in/jocoso5273/" target="_blank" ref={myInputRef} className="px-2 ">
-                        <LiaLinkedin className="w-9 h-auto hover:text-highlightTwo hover:bg-highlightOne rounded-md transition-all ease-in-out delay-150"/>
+            <d id="footer" className="group flex flex-row justify-center h-full bg-gray-300 shadow-lg rounded-b-lg">
+                {/* w-8 h-8 hover:text-highlightTwo */}
+                    <a href="https://www.linkedin.com/in/jocoso5273/" target="_blank" ref={myInputRef} className="p-2 p-5">
+                        <LiaLinkedin className="w-10 h-10 hover:text-highlightTwo hover:bg-highlightOne rounded-md"/>
                     </a>
-                    <a href="https://x.com/Jocoso_Code" target="_blank" ref={myInputRef} className="px-2">
-                        <LiaTwitter className="w-9 h-auto hover:text-highlightTwo hover:bg-highlightOne rounded-md transition-all ease-in-out delay-150"/>
+                    <a href="https://x.com/Jocoso_Code" target="_blank" ref={myInputRef} className="p-2 p-5">
+                        <LiaTwitter className="w-10 h-10 hover:text-highlightTwo hover:bg-highlightOne rounded-md"/>
                     </a>
-                    <a href="https://www.instagram.com/jocoso_code/" target="_blank" ref={myInputRef} className="px-2">
-                        <LiaInstagram className="w-9 h-auto hover:text-highlightTwo hover:bg-highlightOne rounded-md transition-all ease-in-out delay-150"/>
+                    <a href="https://www.instagram.com/jocoso_code/" target="_blank" ref={myInputRef} className="p-5">
+                        <LiaInstagram className="w-10 h-10 hover:text-highlightTwo hover:bg-highlightOne rounded-md"/>
                     </a>
             </d>
 
