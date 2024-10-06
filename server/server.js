c01663dc5819ca72a2c122ffae9af5c929874cf0
jocoso <joshua.collado022@gmail.com>
2024-10-05 23:53:56 -0400

    a

--- server/server.js
+++ server/server.js
@@ -22,7 +22,8 @@ const startApolloServer = async () => {
         app.use(express.urlencoded({ extended: false }));
         app.use(express.json());
         app.use(cors({
-            origin: process.env.FRONTEND_URL || 'http://localhost:3000'
+            origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+            credentials: true,
         }));
         if (process.env.NODE_ENV === "production") {
             app.use(express.static(path.join(__dirname, "../client/dist")));
