f5afe54920c7cf84eab75c7e428504a8aec03e05
jocoso <joshua.collado022@gmail.com>
2024-10-07 03:37:12 -0400

    a

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,18 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 
-// Load environment variables from `.env` file
+// Load environment variables from .env file
 dotenv.config();
 
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: process.env.VITE_PORT || 3000, // Use process.env in the config for Node context
+        port: import.meta.env.VITE_PORT || 3000,
         open: true,
         host: true,
         proxy: {
             "/graphql": {
-                target: process.env.VITE_API_URL, // Use process.env for backend API URL
+                target: import.meta.env.VITE_API_URL || "http://localhost:3001",
                 changeOrigin: true,
                 secure: false,
             },
