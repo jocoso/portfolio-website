6a980d7cc13022e2e3c20c85023eacd47405807f
jocoso <joshua.collado022@gmail.com>
2024-10-07 03:53:12 -0400

    a

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,18 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 
-// Load environment variables from .env file
-dotenv.config();
+dotenv.config(); // Load environment variables
 
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: import.meta.env.VITE_PORT || 3000,
+        port: parseInt(import.meta.env.VITE_PORT, 10) || 3000, // Fallback to 3000 if VITE_PORT is undefined
         open: true,
         host: true,
         proxy: {
             "/graphql": {
-                target: import.meta.env.VITE_API_URL || "http://localhost:3001",
+                target: import.meta.env.VITE_PRODUCTION_URL || "https://portfolio-website-be-9ohl.onrender.com/graphql",
                 changeOrigin: true,
                 secure: false,
             },
