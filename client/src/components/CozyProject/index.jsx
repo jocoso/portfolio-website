8ad3ae6eb88e3b7024ce6747c2614dedbeb37b63
jocoso <joshua.collado022@gmail.com>
2024-10-24 19:46:35 -0400

    Added a condition to the Delete button under projects so it will only display for someone logged.

--- client/src/components/CozyProject/index.jsx
+++ client/src/components/CozyProject/index.jsx
@@ -46,30 +46,32 @@ const CozyProject = ({ data, onDelete }) => {
                     </div>
                 </article>
             </Link>
-            <a
-                onClick={() => onDelete()}
-                className="
-        inline-block
-        px-4
-        py-2
-        mt-10
-        text-white
-        bg-red-600
-        rounded-md
-        shadow
-        hover:bg-red-700
-        hover:shadow-lg
-        focus:outline-none
-        focus:ring-2
-        focus:ring-red-500
-        focus:ring-offset-2
-        transition
-        duration-300
-        cursor-pointer
-    "
-            >
-                Delete
-            </a>
+
+            {isLoggedIn() && (
+                <a
+                    onClick={() => onDelete()}
+                    className="
+                    inline-block
+                    px-4
+                    py-2
+                    mt-10
+                    text-white
+                    bg-red-600
+                    rounded-md
+                    shadow
+                    hover:bg-red-700
+                    hover:shadow-lg
+                    focus:outline-none
+                    focus:ring-2
+                    focus:ring-red-500
+                    focus:ring-offset-2
+                    transition
+                    duration-300
+                    cursor-pointer"
+                >
+                    Delete
+                </a>
+            )}
         </div>
     );
 };
