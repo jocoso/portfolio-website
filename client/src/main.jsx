ce86d9b65c246e9cb35606232f93d0c8b567d03f
jocoso <joshua.collado022@gmail.com>
2024-10-25 12:25:41 -0400

    Optimized main.jsx to use lazy importing more efficiently.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,98 +1,72 @@
+//
 import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// 
+import App from "./App";
+import Error from "./pages/Error";
 
-import App from "./App.jsx";
-import Error from "./pages/Error.jsx"; // Static error page
+// Load in the background.
+const lazyLoad = (Component) => (
+    <Suspense fallback={<div>loading...</div>}>
+        <Component />
+    </Suspense>
+);
 
-
-
-// Lazy load components
-const About = lazy(() => import("./pages/About.jsx"));
-const Art = lazy(() => import("./pages/Art.jsx"));
-const Blog = lazy(() => import("./pages/Blog.jsx"));
-const ContactMe = lazy(() => import("./pages/ContactMe.jsx"));
-const Projects = lazy(() => import("./pages/Projects.jsx"));
-const SinglePost = lazy(() => import("./pages/SinglePost.jsx"));
-const SingleProject = lazy(() => import("./pages/SingleProject.jsx"));
-const Login = lazy(() => import("./pages/Login.jsx"));
+// Importing Pages...
+const About         = lazy(() => import("./pages/About"));
+const Projects      = lazy(() => import("./pages/Projects"));
+const Art           = lazy(() => import("./pages/Art"));
+const Blog          = lazy(() => import("./pages/Blog"));
+const ContactMe     = lazy(() => import("./pages/ContactMe"));
+const Login         = lazy(() => import("./pages/Login"));
+const SinglePost    = lazy(() => import("./pages/SinglePost"));
+const SingleProject = lazy(() => import("./pages/SingleProject"));
 
 // Router configuration
 const router = createBrowserRouter([
     {
-        path: "/",
+        path: '/',
         element: <App />,
         errorElement: <Error />,
         children: [
             {
                 index: true,
-                element: (
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <About />
-                    </Suspense>
-                ),
+                element: lazyLoad(About),
             },
             {
                 path: "/projects",
-                element: (
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Projects />
-                    </Suspense>
-                ),
+                element: lazyLoad(Projects),
             },
             {
                 path: "/art",
-                element: (
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Art />
-                    </Suspense>
-                ),
+                element: lazyLoad(Art),
             },
             {
                 path: "/blog",
-                element: (
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Blog />
-                    </Suspense>
-                ),
+                element: lazyLoad(Blog),
             },
             {
                 path: "/contact-me",
-                element: (
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <ContactMe />
-                    </Suspense>
-                ),
+                element: lazyLoad(ContactMe),
             },
             {
                 path: "/login",
-                element: (
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Login />
-                    </Suspense>
-                ),
+                element: lazyLoad(Login),
             },
             {
                 path: "/post/:postId",
-                element: (
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <SinglePost />
-                    </Suspense>
-                ),
+                element: lazyLoad(SinglePost),
             },
             {
                 path: "/projects/:projectId",
-                element: (
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <SingleProject />
-                    </Suspense>
-                ),
+                element: lazyLoad(SingleProject),
             },
         ],
     },
 ]);
 
-// Rendering the application
+// Rendering the application.
 ReactDOM.createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />
 );
