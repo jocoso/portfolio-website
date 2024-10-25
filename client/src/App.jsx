96643aa9a3ba9fd72951045b9f14c21e7b9e9fca
jocoso <joshua.collado022@gmail.com>
2024-10-25 12:46:05 -0400

    Optimized App.jsx to use the useMemo hook to prevent reinitializing it multiple times when not need be.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,56 +1,67 @@
+//
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { Outlet } from "react-router-dom";
 import { useMemo } from "react";
 
-
+//
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
 import "./App.css";
 
-const uri = import.meta.env.VITE_PRODUCTION_URL ||"http://portfolio-website-w0q7.onrender.com/graphql";
-
-// Apollo Client setup
-const client = new ApolloClient({
-    uri,
-    cache: new InMemoryCache({
-        typePolicies: {
-            Query: {
-                fields: {
-                    // Example: Normalizes items by their id (adjust this based on your schema)
-                    projects: {
-                        keyArgs: false,
-                        merge(existing = [], incoming) {
-                            return [...existing, ...incoming];
+// Apollo Client setup.
+const createApolloClient = (uri) => {
+    return new ApolloClient({
+        uri,
+        cache: new InMemoryCache({
+            typePolicies: {
+                Query: {
+                    fields: {
+                        // Normalizes items by their id.
+                        projects: {
+                            keyArgs: false,
+                            merge(existing = [], incoming) {
+                                return [...existing, ...incoming];
+                            },
                         },
                     },
                 },
             },
-        },
-    }),
-});
+        }),
+    });
+};
 
 function App() {
-    // Memoize navItems to prevent unnecessary recalculations on each render
+    // Either get .env production url or go to default localhost.
+    const uri = import.meta.env.VITE_PRODUCTION_URL || "http://localhost:3001/graphql";
+    // Memoize ApolloClient to avoid re-initialization.
+    const client = useMemo(() => createApolloClient(uri), [uri]);
+    // Memoize navItems to prevent unnecessary recalculations.
     const navItems = useMemo(
         () => [
-            { id: 0, name: "About Me", route: "/" },
-            { id: 1, name: "Projects", route: "/projects" },
-            { id: 2, name: "Art", route: "/art" },
-            { id: 3, name: "Blog", route: "/blog" },
+            { id: 0, name: "About Me"  , route: '/'           },
+            { id: 1, name: "Projects"  , route: "/projects"   },
+            { id: 2, name: "Art"       , route: "/art"        },
+            { id: 3, name: "Blog"      , route: "/blog"       },
             { id: 4, name: "Contact Me", route: "/contact-me" },
         ],
         []
     );
 
-    return (
+    return(
         <ApolloProvider client={client}>
             <div className="min-h-screen min-w-screen flex flex-col bg-background">
+
+                {/* Header */}
                 <Header items={navItems} />
+                
+                {/* Content */}
                 <main className="flex-grow w-11/12 max-w-7xl mx-auto h-full py-10">
                     <Outlet />
                 </main>
+                
+                {/* Footer */}
                 <Footer />
+
             </div>
         </ApolloProvider>
     );
