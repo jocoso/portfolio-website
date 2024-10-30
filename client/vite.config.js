b5cd784ceb28c12572f5fdea68a0089815d034f5
jocoso <joshua.collado022@gmail.com>
2024-10-30 16:38:44 -0400

    Added conditional importing for visualizer and changed default target for graphql to localhost:3001/graphql.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,36 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-import path from "path";
-
-const envFilePath = path.resolve(process.cwd(), ".env");
-const result = dotenv.config({ path: envFilePath });
-
-// Check if environment variables were loaded correctly.
-if (result.error) {
-    console.error("Failed to load .env file", result.error);
-}
 
 export default defineConfig(({ mode }) => {
     const plugins = [react()];
 
-    // Conditionally add visualizer plugin only in development mode.
+    // Only load `rollup-plugin-visualizer` in development mode
     if (mode === "development") {
-        const { visualizer } = require("rollup-plugin-visualizer");
+        const visualizer = require("rollup-plugin-visualizer");
         plugins.push(visualizer());
     }
 
     return {
         plugins,
         server: {
-            port: parseInt(process.env.VITE_PORT, 10) || 3000,
+            port: parseInt(import.meta.env.VITE_PORT, 10) || 3000,
             open: true,
             host: true,
             proxy: {
                 "/graphql": {
                     target:
-                        process.env.VITE_PRODUCTION_URL ||
-                        "http://portfolio-website-be-9ohl.onrender.com/graphql",
+                        import.meta.env.VITE_PRODUCTION_URL ||
+                        "localhost:3001/graphql",
                 },
             },
         },
