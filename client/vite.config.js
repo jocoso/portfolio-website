03eb4a3f17371299aa32779ab5193fa003d5396f
jocoso <joshua.collado022@gmail.com>
2024-10-30 16:20:28 -0400

    Added visualizer as a development tool not executed during production.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,4 @@
-//
 import { defineConfig } from "vite";
-import { visualizer } from "rollup-plugin-visualizer";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 import path from "path";
@@ -8,23 +6,33 @@ import path from "path";
 const envFilePath = path.resolve(process.cwd(), ".env");
 const result = dotenv.config({ path: envFilePath });
 
-// Check if environment variables were loaded correctly
+// Check if environment variables were loaded correctly.
 if (result.error) {
     console.error("Failed to load .env file", result.error);
 }
 
-export default defineConfig({
-    plugins: [react(), visualizer()],
-    server: {
-        port: parseInt(process.env.VITE_PORT, 10) || 3000,
-        open: true,
-        host: true,
-        proxy: {
-            "/graphql": {
-                target:
-                    process.env.VITE_PRODUCTION_URL ||
-                    "http://portfolio-website-be-9ohl.onrender.com/graphql",
+export default defineConfig(({ mode }) => {
+    const plugins = [react()];
+
+    // Conditionally add visualizer plugin only in development mode.
+    if (mode === "development") {
+        const { visualizer } = require("rollup-plugin-visualizer");
+        plugins.push(visualizer());
+    }
+
+    return {
+        plugins,
+        server: {
+            port: parseInt(process.env.VITE_PORT, 10) || 3000,
+            open: true,
+            host: true,
+            proxy: {
+                "/graphql": {
+                    target:
+                        process.env.VITE_PRODUCTION_URL ||
+                        "http://portfolio-website-be-9ohl.onrender.com/graphql",
+                },
             },
         },
-    },
+    };
 });
