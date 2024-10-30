09a69d7de1fe1879841ef4a63cfdf8c5115eedfd
jocoso <joshua.collado022@gmail.com>
2024-10-30 14:59:42 -0400

    Eliminated unused library rollup-plugin-visualizer.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { visualizer } from "rollup-plugin-visualizer";
 import dotenv from "dotenv";
 import path from "path";
 
