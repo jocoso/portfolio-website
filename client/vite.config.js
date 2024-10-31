444394c78209d01498934d901249793ada1ad0ca
jocoso <joshua.collado022@gmail.com>
2024-10-31 00:49:26 -0400

    Replaced process.env... to import.meta.env...

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: parseInt(process.env.PORT, 10) || 3000,
+        // Use import.meta.env.VITE_PORT with a fallback for local development
+        port: parseInt(import.meta.env.VITE_PORT, 10) || 3000,
         open: true,
         host: true,
         proxy: {
             "/graphql": {
-                target: import.meta.env.VITE_PRODUCTION_URL || "http://portfolio-website-be-9ohl.onrender.com/graphql",
+                target: import.meta.env.VITE_PRODUCTION_URL || "http://localhost:3001/graphql",
             },
         },
     },
