26b147cc5aa015a196f3666dc2deb7c275ad3087
jocoso <joshua.collado022@gmail.com>
2025-08-20 13:06:19 -0400

    Added a ScrollLink Component for Contact-Me

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,18 +2,21 @@
 import ScrollLink from "../components/ScrollLink";
 import Avatar from '../assets/portfolio-avatar-img.webp';
 import Art1 from '../assets/art1.png';
+import ContactMeImg from '../assets/contactme.png';
 
 // About Page
 const About = () => {
     return (
         <section className="w-full">
             
-            <ScrollLink src={Avatar} orientation="right" href="/" title="About" className="pb-4">
+            <ScrollLink src={Avatar} orientation="right" href="/" title="About" className="my-4">
                 Want to know more about me and my work? Click here.
             </ScrollLink>
-
-            <ScrollLink src={Art1} orientation="left" href="/" title="Art & Comics">
+            <ScrollLink src={Art1} orientation="left" href="/" title="Art & Comics" className="my-4">
                 Click here to see my art.
+            </ScrollLink>
+            <ScrollLink src={ContactMeImg} orientation="right" href="/" title="Contact Me" className="my-4">
+                Let's talk!
             </ScrollLink>
             
         </section>
